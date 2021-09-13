2c13219b68cabc76f5b98f519c0e7cd9f3b2300e
along <1017065322@qq.com>
2021-09-14 04:44:46 +0800

    两数之和

--- js/01两数之和.js
+++ js/01两数之和.js
@@ -1,4 +1,33 @@
-const hello = (value)=>{
-    console.log(value);
+const numsTotal = (nums, target) => {
+    let list = [];
+    for (let i = 0; i < nums.length; i++) {
+        for (let j = i + 1; j < nums.length; j++) {
+            if (target === nums[i] + nums[j]) {
+                list = [i, j];
+            }
+        }
+    }
+    if (list.length === 0) {
+        // throw Error('无结果');
+        return '无';
+    } else {
+        return list;
+    }
 }
-hello('hello world');
+const nums = [3, 2, 5,4];
+const target = 6;
+// numsTotal(nums, target);
+
+
+const test = (cb, items) => {
+    const startDate = Date.now();
+    for(let i = 0;i < 2; i++){
+        // const result = cb(...items);
+        console.log(1);
+        // console.log('结果：', result);
+    }
+    const endDate = Date.now();
+    console.log('花费时间：', endDate - startDate);
+}
+
+test(numsTotal, [nums, target]);
