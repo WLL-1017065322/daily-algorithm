874118eb23ba7ae61bf662e943d4dd217506359a
along <1017065322@qq.com>
2021-09-25 14:28:21 +0800

    回文修改

--- js/09回文数.js
+++ js/09回文数.js
@@ -3,8 +3,12 @@ const { test } = require('./test');
 const method = (value) => {
     if (value < 0) {
         return false;
+    } else if (value === 0) {
+        return true;
     } else {
+        console.log(value);
         const newValue = value.toString().split("").reverse().join("")
+        console.log("newValue", newValue);
         if (parseInt(newValue) === value) {
             return true;
         } else {
@@ -12,5 +16,5 @@ const method = (value) => {
         }
     }
 }
-const a1 = 321123;
+const a1 = 0;
 test(method, [a1]);
